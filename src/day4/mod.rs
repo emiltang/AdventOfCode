b447252d3ead3365690183c7bd348c0ade2c0e39
Emil Tang Kristensen <emiltangkristensen@gmail.com>
2022-12-04 21:54:15 +0100

    Add day 4 part 2

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -4,7 +4,11 @@ use std::ops::Range;
 struct Elf(Range<i32>);
 
 impl Elf {
-    fn overlap<'a>(a: &'a Elf, b: &'a Elf) -> Option<&'a Elf> {
+    fn overlap_partial(a: &Elf, b: &Elf) -> bool {
+        matches!((a, b), (Elf(x), Elf(y)) if x.end >= y.start && x.start <= y.end)
+    }
+
+    fn overlap_complete<'a>(a: &'a Elf, b: &'a Elf) -> Option<&'a Elf> {
         match (a, b) {
             (Elf(e), Elf(f)) if e.start <= f.start && e.end >= f.end => Some(a),
             (Elf(e), Elf(f)) if e.start >= f.start && e.end <= f.end => Some(b),
@@ -39,11 +43,18 @@ impl Pair {
     }
 
     fn is_overlapping(&self) -> bool {
-        return Elf::overlap(&self.first, &self.second).is_some();
+        return Elf::overlap_complete(&self.first, &self.second).is_some();
     }
 
     fn overlapping_pairs(pairs: &Vec<Pair>) -> Vec<&Pair> {
         pairs.iter().filter(|p| p.is_overlapping()).collect()
+    }
+
+    fn partial_overlapping_pairs(pairs: &Vec<Pair>) -> Vec<&Pair> {
+        return pairs
+            .iter()
+            .filter(|p| Elf::overlap_partial(&p.first, &p.second))
+            .collect();
     }
 }
 
@@ -68,28 +79,70 @@ mod tests {
     fn test_contained() {
         let a = Elf(2..8);
         let b = Elf(3..7);
-        assert_eq!(Some(&a), Elf::overlap(&a, &b))
+        assert_eq!(Some(&a), Elf::overlap_complete(&a, &b))
     }
 
     #[test]
     fn test_contained_2() {
         let a = Elf(2..8);
         let b = Elf(3..9);
-        assert_eq!(None, Elf::overlap(&a, &b))
+        assert_eq!(None, Elf::overlap_complete(&a, &b))
     }
 
     #[test]
     fn test_contained_3() {
         let a = Elf(2..8);
         let b = Elf(1..7);
-        assert_eq!(None, Elf::overlap(&a, &b))
+        assert_eq!(None, Elf::overlap_complete(&a, &b))
     }
 
     #[test]
     fn test_contained_4() {
         let a = Elf(2..8);
         let b = Elf(1..9);
-        assert_eq!(Some(&b), Elf::overlap(&a, &b))
+        assert_eq!(Some(&b), Elf::overlap_complete(&a, &b))
+    }
+
+    #[test]
+    fn test_contained_5() {
+        let a = Elf(2..8);
+        let b = Elf(3..7);
+        assert!(Elf::overlap_partial(&a, &b))
+    }
+
+    #[test]
+    fn test_contained_6() {
+        let a = Elf(2..8);
+        let b = Elf(1..9);
+        assert!(Elf::overlap_partial(&a, &b))
+    }
+
+    #[test]
+    fn test_contained_7() {
+        let a = Elf(2..8);
+        let b = Elf(3..9);
+        assert!(Elf::overlap_partial(&a, &b))
+    }
+
+    #[test]
+    fn test_contained_8() {
+        let a = Elf(2..8);
+        let b = Elf(1..7);
+        assert!(Elf::overlap_partial(&a, &b))
+    }
+
+    #[test]
+    fn test_contained_9() {
+        let a = Elf(2..5);
+        let b = Elf(6..7);
+        assert!(!Elf::overlap_partial(&a, &b))
+    }
+
+    #[test]
+    fn test_contained_10() {
+        let a = Elf(8..10);
+        let b = Elf(1..4);
+        assert!(!Elf::overlap_partial(&a, &b))
     }
 
     #[test]
@@ -101,10 +154,26 @@ mod tests {
     }
 
     #[test]
-    fn test_part_2() {
+    fn test_part_1_complete_set() {
         let lines = util::read("src/day4/input");
         let pairs = Pair::parse_vec(&lines);
         let overlapping_pairs = Pair::overlapping_pairs(&pairs);
         assert_eq!(500, overlapping_pairs.len())
+    }
+
+    #[test]
+    fn test_part_2() {
+        let lines = util::read("src/day4/test");
+        let pairs = Pair::parse_vec(&lines);
+        let overlapping_pairs = Pair::partial_overlapping_pairs(&pairs);
+        assert_eq!(4, overlapping_pairs.len())
+    }
+
+    #[test]
+    fn test_part_2_complete_set() {
+        let lines = util::read("src/day4/input");
+        let pairs = Pair::parse_vec(&lines);
+        let overlapping_pairs = Pair::partial_overlapping_pairs(&pairs);
+        assert_eq!(815, overlapping_pairs.len())
     }
 }
